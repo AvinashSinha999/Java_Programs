cc245c78b82133acfab46380888797b67e2df50f
Avinash Sinha <sinha.avinash906@gmail.com>
2026-06-07 11:17:07 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithNum.java
+++ src/J01_Java_Basic_Programs/Operator_Programs/TernaryOpWithNum.java
@@ -1,3 +1,9 @@
+/*
+  Ternary Operator : A shorter way to write an if/else. It takes a condition, and if the condition is true
+                     it returns the first value, otherwise it returns the second value.
+                     Commonly used to assign a value based on a condition in a single line.
+*/
+
 package J01_Java_Basic_Programs.Operator_Programs;
 
 public class TernaryOpWithNum {
