70108b57046f129c1d5b4a6479938cf67ed9bdaa
Avinash Sinha <sinha.avinash906@gmail.com>
2026-06-27 12:16:36 +0530

    Java Programs

--- src/J02_OOPs_Programs_ClassAndObject/CO11_CarNonStatic.java
+++ src/J02_OOPs_Programs_ClassAndObject/CO11_CarNonStatic.java
@@ -1,3 +1,13 @@
+/*
+    Class:  A class is a blueprint or template used for creating objects.
+            It defines the structure, including the variables (attributes) and methods (behaviors).
+    Object: An object is the real-world entity which is created from that blueprint using the new keyword.
+            It has actual values and can perform actions.
+                                                      OR
+            An object which are accesses and the uses of instance variables and methods
+            which are defined by its class.
+*/
+
 package J02_OOPs_Programs_ClassAndObject;
 
 public class CO11_CarNonStatic {
