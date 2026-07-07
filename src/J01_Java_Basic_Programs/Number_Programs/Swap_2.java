7305583cc55b225a4e446e589c653d4b5731a68a
Avinash Sinha <sinha.avinash906@gmail.com>
2026-07-07 12:27:09 +0530

    Java Programs

--- src/J01_Java_Basic_Programs/Number_Programs/Swap_2.java
+++ src/J01_Java_Basic_Programs/Number_Programs/Swap_2.java
@@ -2,17 +2,17 @@ package J01_Java_Basic_Programs.Number_Programs;
 
 import java.util.Scanner;
 
-public class Swap {
+public class Swap_2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("\n------INPUT VALUES-------");
 
-        System.out.print("\nInput of a value is : ");
+        System.out.print("\nInput of 'a' value is : ");
         int a = sc.nextInt();
 
-        System.out.print("\nInput of b value is : ");
+        System.out.print("\nInput of 'b' value is : ");
         int b = sc.nextInt();
 
         a = a + b;
@@ -21,9 +21,9 @@ public class Swap {
 
         System.out.println("\n------OUTPUT VALUES------");
 
-        System.out.println("\nOutput of a value is : " + a);
+        System.out.println("\nOutput of 'a' value is : " + a);
 
-        System.out.println("\nOutput of b value is : " + b);
+        System.out.println("\nOutput of 'b' value is : " + b);
 
     }
 }
