bd157ffd55d68f062ffb5a2abbba55c34bb35e30
Avinash Sinha <sinha.avinash906@gmail.com>
2026-08-21 23:15:34 +0530

    Java Programs

--- src/J03_OOPs_Programs_Inheritance/IN02_Son.java
+++ src/J03_OOPs_Programs_Inheritance/IN02_Son.java
@@ -1,6 +1,12 @@
-package J03_OOPs_Programs_Inheritance;
+/*
+  Inheritance : A child class inherits properties and methods from a parent class using extends keyword.
 
-//------------------------------ MULTILEVEL INHERITANCE ------------------------------
+  ------------------------------ MULTILEVEL INHERITANCE ------------------------------
+
+  - A child class inherits from a parent, and another class inherits from that child.
+*/
+
+package J03_OOPs_Programs_Inheritance;
 
 public class IN02_Son extends IN02_Father {
 
