34d7267ebb70f75cad46d119a7a909348f00c354
Avinash Sinha <sinha.avinash906@gmail.com>
2026-09-25 22:42:49 +0530

    Java Programs

--- src/J04_OOPs_Programs_Polymorphism/PO01_Class.java
+++ src/J04_OOPs_Programs_Polymorphism/PO01_Class.java
@@ -1,12 +1,14 @@
-package J04_OOPs_Programs_Polymorphism;
-
-//------------------------------ METHOD OVERLOADING ------------------------------
-
 /*
+    Polymorphism: Same method behaving differently based on the object that calls it.
+
+---------------------------------------- METHOD OVERLOADING ----------------------------------------
+
     - When declaring multiple methods on the Same Class with Same Name but different parameters
       is known as Method Overloading.
     - It is achieving in Compile Time Polymorphism or Static Polymorphism.
 */
+
+package J04_OOPs_Programs_Polymorphism;
 
 public class PO01_Class {
 
